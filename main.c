24013c9e3f6b7406477dd8544ce66479cc4e307f
Jack Walsh <jackwals@buffalo.edu>
2026-09-21 20:28:35 -0400

    redid SDL, implemented Fetch and Decode, implemented the Clear Screen and Jump opcodes

--- main.c
+++ main.c
@@ -1,39 +1,38 @@
-#define SDL_MAIN_USE_CALLBACKS 1
-
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
 
 typedef __uint16_t Address;
-static SDL_Window *screen = NULL;
-static SDL_Renderer *renderer = NULL;
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[])
+static bool screen[64][128];
+
+
+int main(int argc, char *argv[])
 {
 	int i, max, c;
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+	int PC = 512;
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+	SDL_Init(SDL_INIT_VIDEO);
 
-    if (!SDL_CreateWindowAndRenderer("Chip-8", 0, 0, SDL_WINDOW_FULLSCREEN, &screen, &renderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+    SDL_CreateWindowAndRenderer("Chip-8", 0, 0, SDL_WINDOW_FULLSCREEN, &window, &renderer);
     SDL_SetRenderLogicalPresentation(renderer, 128, 64, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
 
 	__uint8_t memory[4000];
 
 	printf("Created 4KB memory.\n");
+	printf("Testing bool array: %d\n", screen[4][4]);
 
-	FILE *fp = fopen("../2-ibm-logo.ch8", "rb");
+	FILE *fp = fopen("../../2-ibm-logo.ch8", "rb");
 	if (fp == NULL) {
 		fprintf(stderr, "cannot open input file!\n");
 		return 1;
 	}
 
 	for (i = 0, max = 4000; i < max && (c = getc(fp)) != EOF; i++) {
+		/*
 		printf("%02x", c);
 		if (i % 16 == 15) {
 			putchar('\n');
@@ -41,141 +40,97 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[])
 		else if (i % 4 == 1) {
 			putchar('\n');
 		}
+		*/
 		memory[512 + i] = c;
 	}
 
+	/*
 	printf("Done loading into memory!\n");
 	fclose(fp);
 	for (i = 512; i < 4000; i+= 2){
 		__uint16_t opcode = (memory[i] << 8) | memory[i + 1];
 		printf("%04x\n", opcode);
 	}
+	*/
+	
+	
 
+	bool running = true;
+	int tick = 0;
+	bool jump = false;
+	SDL_Event event;
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_KEY_DOWN) {
+				running = false;
+			}
+		}
+		for (int i = 0; i < 12; i++) {
+			tick++;
+			if (tick > 200) {
+				return 0;
+			}
+			//fetch
+			__uint16_t opcode = (memory[PC] << 8) | memory[PC + 1];
+			if (!jump) {
+				PC += 2;
+			}
+			if (PC >= 1000) {
+				return 0;
+			}
 
-    return SDL_APP_CONTINUE;
-}
+			//decode
+			__uint8_t category = (opcode & 0xF000) >> 12;
+			__uint8_t X = (opcode & 0x0F00) >> 8;
+			__uint8_t Y = (opcode & 0x00F0) >> 4;
+			__uint8_t N = (opcode & 0x000F);
+			__uint8_t NN = (opcode & 0x00FF);
+			__uint16_t NNN = (opcode & 0x0FFF);
+			printf("%x\n", opcode);
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-    }
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+			//execute
+			switch (category) {
+				case 0x0:
+					printf("Clear screen\n");
+					memset(screen, 0, sizeof(screen));
+					break;
+				case 0x1:
+					printf("Jump\n");
+					PC = NNN;
+					jump = true;
+					break;
+				case 0x6:
+					printf("Set register VX\n");
+					break;
+				case 0x7:
+					printf("Add value to register VX\n");
+					break;
+				case 0xA:
+					printf("Set index register I\n");
+					break;
+				case 0xD:
+					printf("Draw\n");
+					break;
+				default:
+					printf("Unknown opcode.\n");
+			}
+		}
 
-}
+		// update display timers
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
+		// update display
 
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer,255,255,255,255);
+		//printf("ClearedDrawColor");
+		SDL_FRect pixel = { 64.0f, 32.0f, 1.0f, 1.0f};
+		SDL_RenderFillRect(renderer, &pixel);
+		SDL_RenderDebugText(renderer, 10, 10, "Chip-8");
+		SDL_RenderPresent(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
-	SDL_SetRenderDrawColor(renderer,255,255,255,255);
-	SDL_FRect pixel = { 64.0f, 32.0f, 1.0f, 1.0f};
-	SDL_RenderFillRect(renderer, &pixel);
-	SDL_RenderDebugText(renderer, 10, 10, "Chip-8");
-	SDL_RenderPresent(renderer);
-	// Memory
-
-	// CHIP-8 will have 4 KB of RAM - more than enough on the Pico board.
-	// The index register and PC can both only store 16 bits.
-	// In order to be compatible with older games, the interpreter should start at 0x200.
-	// Before this address, we'll need to make the font.
-
-	// Font
-
-	// Store the font at 0x050 to 0x09F, following popular convention.
-	// Will want to define macros for the memory locations of the characters.
-	// These will be drawn just like regular sprites.
-
-	// Display
-
-	// CHIP-8 is natively 64x32, but I will be using a 128x64 display (must be scaled up).
-	// The screen is made up of bools. Can I write to each pixel on the display natively?
-	// Only modify the display when an instruction is executed to change something on the display.
-	// Bits in the sprite will logical XOR with the existing ones - will cause flickering.
-
-	// Stack
-
-	// CHIP-8 has a stack, and only uses it to call from subroutines.
-	// It will only store 12 bit addresses.
-	// Limit it to 22 of these addresses, closest to the original limit while stile being whole.
-
-	// Timers
-
-	// There are two timer registers: sound and delay.
-	// As long as they are above 0, they should be deincremented by 1 sixty times a second.
-	// This is where FreeRTOS comes in handy!
-	// If the sound register is above 0, the computer should "beep" (a speaker).
-
-	// Keypad
-
-	// We'll have a 4x4 matrix of buttons that matches it pretty closely design wise.
-	// For a placeholder: we'll use a matrix of the following keystroke signals:
-	// 1 2 3 4
-	// Q W E R
-	// A S D F
-	// Z X C V
-
-	// Fetch/decode/execute loop
-
-	// The main task. It will run in an infinite loop, and:
-	// Fetch the instruction from the PC
-	// Decode the instruction to find out what to do
-	// Execute the instruction and do what it tells you
-	// Will likely want to do around 700 instructions a second.
-
-	// Fetch
-
-	// Read the two bytes the PC is pointing at.
-	// Immediately increment the PC by two.
-
-	// Decode
-
-	// Use switch statements, mask off the first hex number in the instruction.
-	// Have one case per number, use break; so it doesn't start falling through.
-	// X: the second nibble. Used to look up 1 of 16 registers.
-	// Y: the third nibble. Same case as above.
-	// N: the fourth nibble. A four bit number.
-	// NN: second byte. 8-bit immediate number.
-	// NNN: second, third, fourth nibbles. 12-bit memory address.
-	// Extract these before decoding to avoid redundancy.
-	// Use macros to make this easier!
-
-	// Execute
-
-	// Start with: clear screen, jump, set register, add value to reg, set index reg, display.
-	// Can test these with the IBM logo program.
-	// 00E0: Clear Screen
-	// 1NNN: Jump
-	// 00EE and 2NNN: Subroutines
-	// 3XNN, 4XNN, 5XY0, 0XY0: Skip Conditionally
-	// 6XNN: Set
-	// 7XNN: Add
-	// 8XY0: Set
-	// 8XY1: Binary OR
-	// 8XY2: Binary AND
-	// 8XY3: Binary SOR
-	// 8XY4: Add
-	// 8XY5 and 8XY7: Subtract
-	// 8XY6 and 8XYE: Shift
-	// ANNN: Set Index
-	// BNNN: Jump with offset
-	// CXNN: Random
-	// DXYN: Display
-	// EX9E and EXA1: Skip if key
-	// FX07,FX15 and FX18: Timers
-	// FX1E: Add to index
-	// FX0A: Get Key
-	// FX29: Font Character
-	// FX33: Binary Coded decimal conversion
-	// FX55 and FX65: Store and load memory
-	return SDL_APP_CONTINUE;
-}
-
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-
+		// delay
+	}	
+	return 0;
 }
 
