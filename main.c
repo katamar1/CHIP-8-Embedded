c5e67607f0fe6a45b9f33632991396170792e449
Jack Walsh <jackwals@buffalo.edu>
2026-09-23 22:31:14 -0400

    basic functionality for the IBM rom implemented, as well as build tooling

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 
 typedef __uint16_t Address;
 
-static bool screen[64][128];
+static bool screen[32][64];
+
+static __uint8_t registers[16];
+
+static __uint16_t index_register;
 
 
 int main(int argc, char *argv[])
@@ -17,7 +21,7 @@ int main(int argc, char *argv[])
 	SDL_Init(SDL_INIT_VIDEO);
 
     SDL_CreateWindowAndRenderer("Chip-8", 0, 0, SDL_WINDOW_FULLSCREEN, &window, &renderer);
-    SDL_SetRenderLogicalPresentation(renderer, 128, 64, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
 
 	__uint8_t memory[4000];
@@ -43,6 +47,7 @@ int main(int argc, char *argv[])
 		*/
 		memory[512 + i] = c;
 	}
+	fclose(fp);
 
 	/*
 	printf("Done loading into memory!\n");
@@ -56,20 +61,29 @@ int main(int argc, char *argv[])
 	
 
 	bool running = true;
-	int tick = 0;
 	bool jump = false;
 	SDL_Event event;
+	int jump_count = 0;
 	while (running) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_EVENT_KEY_DOWN) {
 				running = false;
 			}
 		}
-		for (int i = 0; i < 12; i++) {
-			tick++;
-			if (tick > 200) {
-				return 0;
+		if (jump_count > 10) {
+			for (int l = 0; l < 32; l++) {
+				for (int k = 0; k < 64; k++) {
+					if (screen[l][k] == 0) {
+						printf(". ");
+					} else {
+						printf("%u ", screen[l][k]);
+					}
+				}
+				printf("\n");
 			}
+			running = false;
+		}
+		for (int m = 0; m < 12; m++) {
 			//fetch
 			__uint16_t opcode = (memory[PC] << 8) | memory[PC + 1];
 			if (!jump) {
@@ -98,18 +112,53 @@ int main(int argc, char *argv[])
 					printf("Jump\n");
 					PC = NNN;
 					jump = true;
+					jump_count++;
 					break;
 				case 0x6:
 					printf("Set register VX\n");
+					registers[X] = NN;
 					break;
 				case 0x7:
 					printf("Add value to register VX\n");
+					if (registers[X] + NN > 0xFF) {
+						break;
+					} else {
+						registers[X] += NN; 
+					}
 					break;
 				case 0xA:
 					printf("Set index register I\n");
+					index_register = NNN;
 					break;
 				case 0xD:
 					printf("Draw\n");
+					__uint8_t x_coord = registers[X] % 64;
+					__uint8_t x_coord_original = x_coord;
+					__uint8_t y_coord = registers[Y] % 32;
+					registers[0xF] = 0;
+					for (int i = 0; i < N; i++) {
+						if (y_coord > 32) {
+							break;
+						}
+						x_coord = x_coord_original;
+						__uint8_t sprite_data = memory[index_register + i];
+						__uint8_t temp_bit_helper = 0x80;
+						for (int j = 7; j >= 0; j--) {
+							if (x_coord > 64) {
+								break;
+							}
+							__uint8_t pixel = (sprite_data & (temp_bit_helper >> (7 - j))) >> j;
+							if (pixel == 1 && screen[y_coord][x_coord] == 1) {
+								screen[y_coord][x_coord] = 0;
+								registers[0xF] = 1;
+							} else if (pixel == 1 && screen[y_coord][x_coord] == 0) {
+								screen[y_coord][x_coord] = 1;
+							}
+							
+							x_coord++;
+						}
+						y_coord++;
+					}
 					break;
 				default:
 					printf("Unknown opcode.\n");
